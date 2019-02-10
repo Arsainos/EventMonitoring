18f491add2f0dac65a44fb4f80c74b3f907aa11f
Belyavtsev Artyem <arsainos@gmail.com>
2019-02-10 16:20:00 +0300

    Add some classes

---
+++ EventMonitoring/Utlity/QueueUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMonitoring.Utlity
+{
+    class QueueUtils
+    {
+        private Dictionary<string, Queue> queuePool = new Dictionary<string, Queue>();
+
+        public void setQueuePool(string key)
+        {
+            queuePool.Add(key, new Queue());
+        }
+
+        public Queue getQueueFromPool(string key)
+        {
+            try
+            {
+                return queuePool[key];
+            }
+            catch
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+    }
+}
---
+++ EventMonitoring/Utlity/SyncQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMonitoring.Utlity
+{
+    class SyncQueue
+    {
+        private Queue queue;
+        private object lockObject;
+
+        public SyncQueue(object Lock)
+        {
+            queue = new Queue();
+            lockObject = Lock;
+        }
+
+        private object Dequeue()
+        {
+            lock(lockObject)
+            {
+                return queue.Dequeue();
+            }           
+        }
+
+        private bool CanDequeue()
+        {
+            if(queue.Peek() == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void SafeEnqueue(object input)
+        {
+            lock(lockObject)
+            {
+                queue.Enqueue(input);
+            }
+        }
+
+        public object SafeDequeue()
+        {
+            if(CanDequeue())
+            {
+                return Dequeue();
+            }
+
+            return null;
+        }
+    }
+}
